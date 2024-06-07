d4f254ddc8ba2dc1b7d416f8bcde9f2e5cefd630
Rodolfo Miranda <rodolfomirandayt@gmail.com>
2024-06-07 12:43:39 -0300

    uptd-transacao

--- src/main/java/CoinKeeper/model/Transacao.java
+++ src/main/java/CoinKeeper/model/Transacao.java
@@ -3,12 +3,11 @@ package CoinKeeper.model;
 import java.time.LocalDate;
 import java.util.UUID;
 
-import com.vaadin.flow.component.template.Id;
-
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.AccessLevel;
