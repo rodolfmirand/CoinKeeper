56d3ba7d253c20110eb25922e7753d6937c546a7
Rodolfo Miranda <rodolfomirandayt@gmail.com>
2024-06-07 12:19:05 -0300

    crtd-categoriaRequestDTO

--- src/main/java/CoinKeeper/service/CategoriaServiceImplements.java
+++ src/main/java/CoinKeeper/service/CategoriaServiceImplements.java
@@ -1,5 +1,15 @@
 package CoinKeeper.service;
 
-public class CategoriaServiceImplements {
+import java.util.UUID;
+
+import dto.response.CategoriaResponseDTO;
+
+public interface CategoriaServiceImplements {
+
+    CategoriaResponseDTO findById(UUID id);
+
+    List<CategoriaResponseDTO> findAll();
+
+    CategoriaResponseDTO register()
     
 }
---
+++ src/main/java/dto/request/CategoriaRequestDTO.java
@@ -0,0 +1,11 @@
+package dto.request;
+
+import lombok.Getter;
+
+@Getter
+public class CategoriaRequestDTO {
+
+    private String nome;
+
+    private String descricao;
+}
