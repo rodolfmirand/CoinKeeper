40c8192ae1a06692682388c8beffcacbb2867441
Rodolfo Miranda <rodolfomirandayt@gmail.com>
2024-06-17 12:45:22 -0300

    uptd&crtd
    uptd - jwtUtils
    crtd - UserDetailsImpl

--- src/main/java/CoinKeeper/security/jwt/JwtUtils.java
+++ src/main/java/CoinKeeper/security/jwt/JwtUtils.java
@@ -1,5 +1,65 @@
 package CoinKeeper.security.jwt;
 
+import java.nio.charset.MalformedInputException;
+import java.security.Key;
+import java.util.Date;
+
+import javax.crypto.SecretKey;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import CoinKeeper.service.UserDetailsImpl;
+import io.jsonwebtoken.ExpiredJwtException;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.MalformedJwtException;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.UnsupportedJwtException;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+
+@Component
 public class JwtUtils {
-    
+
+    @Value("${CoinKeeper.jwtSecret}")
+    private String jwtSecret; // password
+
+    @Value("${CoinKeeper.jwtExpirationMs}")
+    private int jwtExpirationMs; // tempo que o token vai ficar válido
+
+    public String generateTokenFromuserDetailsImp(UserDetailsImpl userDetails) {
+        return Jwts.builder()
+                .setSubject(userDetails.getUsername())
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(new Date().getTime() + jwtExpirationMs))
+                .signWith(getSigningKey(), SignatureAlgorithm.HS512).compact();
+    }
+
+    private Key getSigningKey() {
+        SecretKey key = Keys.hmacShaKeyFor(Decoders.BASE64.decode(jwtSecret));
+        return key;
+    }
+
+    private boolean validateJwtToken(String authToken) {
+        
+        try {
+            Jwts.parser().setSigningKey(getSigningKey()).build().parseClaimsJws(authToken);
+            return true;
+
+        } catch (MalformedJwtException e) {
+            System.out.println("Token inválido. " + e.getMessage());
+
+        } catch (ExpiredJwtException e) {
+            System.out.println("Token expirado. " + e.getMessage());
+
+        } catch (UnsupportedJwtException e) {
+            System.out.println("Token não suportado. " + e.getMessage());
+
+        } catch (IllegalArgumentException e) {
+            System.out.println("Argumento inválido. " + e.getMessage());
+
+        }
+
+        return false;
+    }
 }
---
+++ src/main/java/CoinKeeper/service/UserDetailsImpl.java
@@ -0,0 +1,36 @@
+package CoinKeeper.service;
+
+import java.util.Collection;
+import java.util.UUID;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+public class UserDetailsImpl implements UserDetails {
+
+    private UUID id;
+
+    private String userName;
+
+    private String email;
+
+    private String password;
+
+    private Collection<? extends GrantedAuthority> authorities;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return this.authorities;
+    }
+
+    @Override
+    public String getPassword() {
+        return this.password;
+    }
+
+    @Override
+    public String getUsername() {
+        return this.userName;
+    }
+    
+}
