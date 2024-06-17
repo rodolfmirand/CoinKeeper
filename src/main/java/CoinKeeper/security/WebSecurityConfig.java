b05de9800ff57af9b718c43be38ac0e952008be9
Rodolfo Miranda <rodolfomirandayt@gmail.com>
2024-06-17 12:18:48 -0300

    crtd-webSecurityConfig

---
+++ src/main/java/CoinKeeper/security/WebSecurityConfig.java
@@ -0,0 +1,10 @@
+package CoinKeeper.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+
+@Configuration
+@EnableMethodSecurity
+public class WebSecurityConfig {
+    
+}
