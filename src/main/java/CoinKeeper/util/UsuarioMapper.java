dde2eeca87f0de1a8dfd4d1b61a3771b485d4107
Rodolfo Miranda <rodolfomirandayt@gmail.com>
2024-06-17 14:33:02 -0300

    updt-usuarioMapper-passwordEncoder

--- src/main/java/CoinKeeper/util/UsuarioMapper.java
+++ src/main/java/CoinKeeper/util/UsuarioMapper.java
@@ -3,6 +3,8 @@ package CoinKeeper.util;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 
 import CoinKeeper.dto.request.UsuarioRequestDTO;
@@ -11,12 +13,15 @@ import CoinKeeper.model.Usuario;
 
 @Component
 public class UsuarioMapper {
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
     
     public Usuario toUsuario(UsuarioRequestDTO userDTO) {
         return Usuario.builder()
                 .nome(userDTO.getNome())
                 .email(userDTO.getEmail())
-                .senha(userDTO.getSenha())
+                .senha(passwordEncoder.encode(userDTO.getSenha()))
                 .build();
     }
 
