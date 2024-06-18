654ca044878b1384a74fbd6ffc2bb7f95bdf7dd2
Rodolfo Miranda <rodolfomirandayt@gmail.com>
2024-06-18 13:35:09 -0300

    crtd-EnumSituacaoUsuario

--- src/main/java/CoinKeeper/model/Usuario.java
+++ src/main/java/CoinKeeper/model/Usuario.java
@@ -2,9 +2,12 @@ package CoinKeeper.model;
 
 import java.util.UUID;
 
+import CoinKeeper.model.enums.SituacaoUsuario;
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -44,6 +47,9 @@ public class Usuario {
 
     @OneToOne(cascade = CascadeType.ALL)
     private Conta conta;
+
+    @Enumerated(EnumType.STRING)
+    private SituacaoUsuario situacao;
 
     @Builder
     public Usuario(String nome, String login, String email, String senha, Conta conta) {
