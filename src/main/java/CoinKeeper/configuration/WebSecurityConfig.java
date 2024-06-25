3d2d407be0f3de367624e0bdf046efbb54826461
Rodolfo Miranda <rodolfomirandayt@gmail.com>
2024-06-25 14:30:32 -0300

    uptd - userDetailsImpl

--- src/main/java/CoinKeeper/configuration/WebSecurityConfig.java
+++ src/main/java/CoinKeeper/configuration/WebSecurityConfig.java
@@ -7,6 +7,7 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -18,6 +19,7 @@ import CoinKeeper.configuration.jwt.AuthFilterToken;
 
 @Configuration
 @EnableMethodSecurity
+@EnableWebSecurity
 public class WebSecurityConfig {
 
     @Autowired
--- src/main/java/CoinKeeper/service/usuario/userDetails/UserDetailsImpl.java
+++ src/main/java/CoinKeeper/service/usuario/userDetails/UserDetailsImpl.java
@@ -1,6 +1,5 @@
 package CoinKeeper.service.usuario.userDetails;
 
-import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 import java.util.UUID;
