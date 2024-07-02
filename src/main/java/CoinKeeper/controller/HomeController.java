ddac697f24c59461eabf405eee9f79afb772f5ae
Rodolfo Miranda <rodolfomirandayt@gmail.com>
2024-07-02 14:12:51 -0300

    update - homeController

--- src/main/java/CoinKeeper/controller/HomeController.java
+++ src/main/java/CoinKeeper/controller/HomeController.java
@@ -2,12 +2,14 @@ package CoinKeeper.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+@RequestMapping("/coinkeeper")
 @Controller
 public class HomeController {
     
-    @GetMapping("/")
+    @GetMapping("/home")
     public ModelAndView index() {
         ModelAndView mv = new ModelAndView();
         mv.setViewName("home/index");
