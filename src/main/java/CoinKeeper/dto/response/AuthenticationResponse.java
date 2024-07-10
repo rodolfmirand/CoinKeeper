297e25d6ca7e458c6e100a51422c6be9168dc728
Rodolfo Miranda <rodolfomirandayt@gmail.com>
2024-07-10 12:10:58 -0300

    update

--- src/main/java/CoinKeeper/dto/response/AuthenticationResponse.java
+++ src/main/java/CoinKeeper/dto/response/AuthenticationResponse.java
@@ -7,8 +7,6 @@ public class AuthenticationResponse {
     
     private String token;
 
-    // TODO implementar retornar o usuario e liberacoes (authorities)
-
     public AuthenticationResponse(String token) {
         super();
         this.token = token;
